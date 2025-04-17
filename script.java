814180efd2be9b4db1046ed428462b7a5e886d33
wanzel99 <wanzellima2@gmail.com>
2025-04-16 21:18:56 -0300

    script.java

--- script.java
+++ script.java
@@ -2,3 +2,10 @@ let display = document.getElementById("display");
 function adicionarValor(valor) {
     display.value += valor;
 }
+function calcular() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+    }
+}
